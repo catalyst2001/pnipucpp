2db36cdf9e9aa9bfeb3075de810052d599f4f062
catalyst <39456203+catalyst2001@users.noreply.github.com>
2024-01-20 17:34:32 +0500

    update 12labk

--- 2c-4sem/csharp/lab11i/obj/Debug/net8.0/lab11i.AssemblyInfo.cs
+++ 2c-4sem/csharp/lab11i/obj/Debug/net8.0/lab11i.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab11i")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9653ff6afabbb6db68145884ce31866baae727b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab11i")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab11i")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 2c-4sem/csharp/lab12k/Lab124.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12k
+{
+    public class Lab124
+    {
+        public void Start() {
+            MyCollectionQueue<int> myQueue = new MyCollectionQueue<int>();
+            if (myQueue.IsEmpty())
+                Console.WriteLine("collection is empty\n");
+
+            for(int i = 0; i < 10; i++) {
+                myQueue.PushBack(i);
+                Console.WriteLine("Pushed {0} data to queue\n", i);
+            }
+
+            Console.WriteLine("--- printing by foreach ---");
+            foreach (var data in myQueue)
+                Console.WriteLine("Data in myQueue: {0}", data);
+
+            if (!myQueue.IsEmpty())
+            {
+                while (!myQueue.IsEmpty())
+                {
+                    int dataFromQueue = myQueue.Front();
+                    Console.WriteLine("Readed from queue: {0}", dataFromQueue);
+                }
+            }
+
+
+        }
+    }
+}
