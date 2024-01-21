35a21408e8d8d4f4352e8f466332db3545220b94
catalyst <39456203+catalyst2001@users.noreply.github.com>
2024-01-21 18:59:33 +0500

    update lab13k

---
+++ 2c-4sem/csharp/lab13k2/MyCollection.cs
@@ -0,0 +1,49 @@
+﻿using lab10;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab13k2
+{
+    public class MyCollection : MyCollectionQueue<Organization>
+    {
+        public MyCollection() : base() {
+        }
+
+        public MyCollection(int cap) : base(cap) {
+        }
+
+        public void FillCollection() {
+            MyCollectionNode<Organization>? node = tail;
+            while (node != null) {
+                node.Data.AutoFill();
+                node = node.Next;
+            }
+        }
+
+        public void Sort() {
+            MyCollectionNode<Organization>? node = tail;
+            List<Organization> list = new List<Organization>(count);
+            while (node != null) {
+                list.Add(node.Data);
+                node = node.Next;
+            }
+
+            list.Sort();
+
+            node = tail;
+            foreach (var item in list) {
+                Debug.Assert(node != null, "node can never be null here!");
+                node.Data = item;
+                node = node.Next;
+            }
+        }
+
+        public void Clear() {
+            Free();
+        }
+    }
+}
---
+++ 2c-4sem/csharp/lab13k2/MyNewCollection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab13k2
+{
+    public class CollectionHandlerEventArgs : System.EventArgs {
+    }
+
+    public delegate void CollectionHandler(object source, CollectionHandlerEventArgs args);//делегат
+
+    public class MyNewCollection : MyCollection
+    {
+        public event CollectionHandler CollectionCountChanged;
+        public event CollectionHandler CollectionReferenceChanged;
+
+        public void CollectionCountChanged(object sourse, CollectionHandlerEventArgs e)
+        {
+            JournalEntry je = new JournalEntry(e.NameCollection, e.ChangeCollection, e.Obj.ToString());
+            journal.Add(je);
+
+        }
+        public void CollectionReferenceChanged(object sourse, CollectionHandlerEventArgs e)
+        {
+            JournalEntry je = new JournalEntry(e.NameCollection, e.ChangeCollection, e.Obj.ToString());
+            journal.Add(je);
+        }
+
+        //обработчик события CollectionCountChanged
+        public virtual void OnCollectionCountChanged(object source, CollectionHandlerEventArgs args)
+        {
+            if (CollectionCountChanged != null)
+                CollectionCountChanged(source, args);
+        }
+        //обработчик события OnCollectionReferenceChanged
+        public virtual void OnCollectionReferenceChanged(object source, CollectionHandlerEventArgs args)
+        {
+            if (CollectionReferenceChanged != null)
+                CollectionReferenceChanged(source, args);
+        }
+
+        public override bool Remove(int position)
+        {
+            OnCollectionCountChanged(this, new CollectionHandlerEventArgs(this.Name, "delete", list[position]));
+            return base.Remove(position);
+        }
+        public override int Add(Person p)
+        {
+            OnCollectionCountChanged(this, new CollectionHandlerEventArgs(this.Name, "add", p));
+            return base.Add(p);
+        }
+        public override Person this[int index]
+        {
+            get
+            {
+                return base[index];
+            }
+            set
+            {
+                OnCollectionReferenceChanged(this, new CollectionHandlerEventArgs(this.Name, "changed", list[index]));
+                base[index] = value;
+            }
+        }
+
+
+    }
+}
